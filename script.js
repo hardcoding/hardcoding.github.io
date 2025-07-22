4955f1abb2549dd39de503c7ee58031233b6fbb5
Johnny <77633762+hardcoding@users.noreply.github.com>
2025-07-22 17:27:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -185,8 +185,8 @@ const init = {
     navigator.mediaDevices.getUserMedia({ video: true })
       .then(camera.list)
       .catch(() => alert(
-        "Es konnte nicht auf die Kamera zugegriffen werden."
-        + "\n Mögliche Fehlerursachen:" +
+        "Es konnte nicht auf die Kamera zugegriffen werden." 
+        + "\n Mögliche Fehlerursachen:"
         + "\n - es ist keine Kamera angeschlossen"
         + "\n - der Browser hat vom OS nicht die Berechtigung, auf die Kamera zuzugreifen"
         + "\n - die Webseite hat vom Browser nicht die Berechtigung, auf die Kamera zuzugreifen"
